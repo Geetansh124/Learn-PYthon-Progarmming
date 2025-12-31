758a4be1fb1df491c35dd372ca2920dfa670ecfc
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2025-12-31 22:26:52 +0530

    main

--- Python Programing/Chapter-6/Practice Set-6/Problem-02.py
+++ Python Programing/Chapter-6/Practice Set-6/Problem-02.py
@@ -6,8 +6,8 @@ marks3 = int(input("Enter Marks for Sbject 3: "))
 
 total_percentage = (100*(marks1 + marks2 + marks3))/300
 
-if(total_percentage>=40):
-    print("You are Passed")
+if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("You are Passed", total_percentage)
 
 else:
-    print("You Failed")    
+    print("You Failed", total_percentage)    
