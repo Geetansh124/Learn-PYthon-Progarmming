c5222e502aa76c7be6e14ed4cecb85a12849bc98
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2026-01-01 19:38:41 +0530

    main

---
+++ Python Programing/Chapter-7/01_Quick_Quiz.py
@@ -0,0 +1,5 @@
+i = 0
+
+while i < 51:
+    print("Geetansh", i)
+    i += 1 # also we can use ->  i = i + 1
---
+++ Python Programing/Chapter-7/03_list-using-while..py
@@ -0,0 +1,7 @@
+l =[1, 5.6, False, "python", "Geetansh", ".Py", "Here is the code"]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i += 1
