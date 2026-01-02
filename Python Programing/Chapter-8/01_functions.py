41cf5da42e4f159ce18c5e035d4947edf924e26e
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2026-01-02 20:58:06 +0530

    main

---
+++ Python Programing/Chapter-8/01_functions.py
@@ -0,0 +1,26 @@
+# a = int(input("Enter Your Number:" ))
+# b = int(input("Enter Your Number:" ))
+# c = int(input("Enter Your Number:" ))
+
+# average = (a + b + c)/3
+# print(average)
+
+# Function Definition
+def avg():
+    a = int(input("Enter Your Number:" ))
+    b = int(input("Enter Your Number:" ))
+    c = int(input("Enter Your Number:" ))
+
+    average = (a + b + c)/3
+    print("This is the Average")
+    print(average)
+    print("Thank You")
+    
+avg() # Function Call
+print("Bye..")
+avg() # Function Call
+print("Bye..")
+avg() # Function Call
+print("Bye..")
+avg() # Function Call   
+print("Bye..")
---
+++ Python Programing/Chapter-8/03_function-with-arguments.py
@@ -0,0 +1,7 @@
+def goodday(name, ending):
+    print("Good Day " + name)
+    print((ending))
+    return "h"
+
+a = goodday("Geetansh", "Thank You")    
+print(a)
