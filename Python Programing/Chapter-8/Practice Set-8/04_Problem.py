2bcec92d5be49b8aa5788c5787f7eeb3cb820fc1
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2026-01-03 14:54:57 +0530

    main

--- Python Programing/Chapter-8/Practice Set-8/04_Problem.py
+++ Python Programing/Chapter-8/Practice Set-8/04_Problem.py
@@ -1,8 +1,12 @@
 '''
 sum(1) = 1
 sum(1) = 1 + 2
-sum(1) = 1 + 2 + 3 + 4...n
+sum(1) = 1 + 2 + 3 + 4...n -1, +n   
 
 '''
+def sum(n):
+    if(n==1):
+        return 1
+    return sum(n-1) + n
 
-jjjjj
+print(sum(4))
